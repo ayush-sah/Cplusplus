40b16dfe326656f2b1a361c8dc4012c72a85e4f9
ayush-sah <ayush.sah@spit.ac.in>
2020-01-30 01:08:27 +0530

    update

--- Searching and Hashing/digit_extraction.cpp
+++ Searching and Hashing/digit_extraction.cpp
@@ -1,12 +1,10 @@
 #include<iostream>
 #include<math.h>
-
 using namespace std;
 
 class hashing{
     private:
-        int n, size, *arr, input;
-        unsigned long int index;
+        int n, size, *arr, input, index, *loc, no_of_digits_locations;
     public:
         hashing(){
             do{
@@ -15,32 +13,37 @@ class hashing{
                 cout << "Enter the no. of digits you want to insert: ";
                 cin >> n;
             }while(n>size);
-            
+            arr = new int[size];
+            cout << "\nEnter the no. of digits you want to extract: ";
+            cin >> no_of_digits_locations;
+            loc = new int[no_of_digits_locations];
+            for(int i = 0; i < no_of_digits_locations; i++){
+                cout << "Enter location no. " << i+1 << " you want to extract: ";
+                cin >> loc[i];
+            }
+            sort_array();
+
             arr = new int[size];
 
             for(int i = 0; i < size; i++)
                 arr[i] = -1;
         }
 
-        void midsquare(){
+        void modulo(){
             cout << endl;
             while(n--){
+                index = 0;
                 cout << "Enter value you want to insert: ";
                 cin >> input;
-                
+
                 if (input < 0){
                     cout << "Invalid Input. Try Again\n";
                     n++;continue;
                 }
 
-                index = input*input;
-
-                while(int(log10(index)+1)>2){
-                    index /= 10;
-                    index %= int(pow(10.0,float(int(log10(index)))));
-                    if(index<size)
-                        break;
-                }
+                for(int i = 0; i < no_of_digits_locations; i++)
+                    index += ((input % int(pow(10.0,float(loc[i]))))/(int(pow(10.0,float(loc[i])))/10))*int(pow(10.0,float(i)));
+                
 
                 if(index>=size)
                     index = index%size;
@@ -51,10 +54,21 @@ class hashing{
                     else
                         index = 0;
                 }
-
                 arr[index] = input;
             }
             display();
+        }
+        
+        void sort_array(){
+            for(int i = 0; i < no_of_digits_locations; i++){
+                for(int j = 0; j < (no_of_digits_locations-i)-1; j++){
+                    if(arr[j]>arr[j+1]){
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+            }
         }
 
         void display(){
@@ -72,5 +86,5 @@ class hashing{
 
 int main(){
     hashing ob;
-    ob.midsquare();
+    ob.modulo();
 }
