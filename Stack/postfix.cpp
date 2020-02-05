71d41b5fdc08fc02033b9dba430153da7ab3c241
ayush-sah <ayush.sah@spit.ac.in>
2020-02-06 00:35:10 +0530

    Update postfix.cpp

--- Stack/postfix.cpp
+++ Stack/postfix.cpp
@@ -12,29 +12,54 @@ class stack{
             top = -1;
             if ((isNumber((str.at(0) - '0'))==true) && (isNumber((str.at(1) - '0'))==true)){
                 arr = new int[str.length()];
-                for(i = 0; i < str.length(); i++){
-                    temp = 0;
-                    while(str.at(i)!=' '){
-                        temp += str.at(0) - '0';
+                for(i = 0; str.at(i) != ')'; i++){
+                    if(isNumber(str.at(i) - '0')==true){
+                        push(str.at(i) - '0');
                     }
-                    cout << temp;
+                    else if((str.at(i)=='+')||(str.at(i)=='-')||(str.at(i)=='*')||(str.at(i)=='/')){
+                        int a = pop();
+                        int b = pop();
+                        switch (str.at(i)){
+                            case '+':
+                                push(b+a);
+                                break;
+                            case '-':
+                                push(b-a);
+                                break;
+                            case '*':
+                                push(b*a);
+                                break;
+                            case '/':
+                                push(b/a);
+                                break;
+                            
+                            default:
+                                break;
+                        }
+                    }
+                }
+                if(top == 0){
+                    cout << "Answer= " << arr[0];
+                }
+                else{
+                    cout << "Wrong Postfix operation.";
                 }
             }
             else
                 cout << "Not a Postfix expression.\n";
         }
         void push(int n){
-            if(top==str.length()-1);
-            else{
+            if(top==str.length()-1)
+                cout << "Error push";
+            else
                 arr[++top] = n;
-            }
         }
 
-        void pop(){
-            if(top==-1);
-            else{
-                top--;
-            }
+        int pop(){
+            if(top==-1)
+                cout << "Error pop";
+            else
+                return arr[top--];
         }
         bool isNumber(int n){
             if ((n == 0)||(n == 1)||(n == 2)||(n == 3)||(n == 4)||(n == 5)||(n == 6)||(n == 7)||(n == 8)||(n == 9))
