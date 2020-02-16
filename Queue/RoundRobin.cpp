41c228dadc39ba560825ab87aeb29ac1bfa4791b
ayush-sah <ayush.sah@spit.ac.in>
2020-02-16 13:14:06 +0530

    Update RoundRobin.cpp

--- Queue/RoundRobin.cpp
+++ Queue/RoundRobin.cpp
@@ -10,36 +10,62 @@ using namespace std;
 */
 
 class queue{
-    int **rr, num, tq, min, time;
+    int **rr, num, tq, min, time, tbt, i, *cirq, front, rear;
     public:
     queue(){
+        front = rear = -1;
         cout << "Enter the number of process: ";
         cin >> num;
         rr = new int*[num];
         tbt = 0;
-        for(int i = 0; i < num; i++){
+        for(i = 0; i < num; i++){
             rr[i] = new int[4];
             cout << "Enter the arrival time for Process" << i+1 << ":";
             cin >> rr[i][0];
             cout << "Enter the burst time for Process" << i+2 << ":";
             cin >> rr[i][1];
-            rr[i][2] rr[i][1];
+            rr[i][2] = rr[i][1];
             tbt += rr[i][1];
-            cout << "Enter time quantum: ";
-            cin >> tq;
+            rr[i][3] = {0};
         }
-        rr[i][3] = {0};
-        q = new int[num];
-        while(tbt!=0){
-            
+        cout << "Enter time quantum: ";
+        cin >> tq;
+        cout << min;
+        time = minimum(rr[0]);
+        cirq = new int[num];
+        while(time<tbt){
+            for(i = 0; i < num; i++){
+                if(rr[0][i] <= time)
+                    enqueue(i);
+            }
+            for()
         }
     }
     int minimum(int *a){
         min = a[0];
-        for(int i = 0; i < a.length(); i++){
-            if (min > a[i])
-                min = a[i];
+        for(int j = 0; j < sizeof(a)/sizeof(int); j++){
+            if (min > a[j])
+                min = a[j];
         }
+    }
+    void enqueue(int a){
+        if(front == -1){
+            front = rear = 0;
+            cirq[rear] = a;
+        }
+        else{
+            ++rear %= num;
+            cirq[rear] = a;
+        }
+    }
+    void dequeue(int a){
+        if(rear==front){
+            rear = front = -1;
+        }
+        else{
+            ++front %= num;
+        }
+        
     }
 
 };
