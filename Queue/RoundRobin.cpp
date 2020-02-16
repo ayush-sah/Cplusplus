0f7090fb5dba5923ac4fcb2d29fa0ead1ac48b06
ayush-sah <ayush.sah@spit.ac.in>
2020-02-16 18:28:16 +0530

    Update RoundRobin.cpp

--- Queue/RoundRobin.cpp
+++ Queue/RoundRobin.cpp
@@ -4,13 +4,14 @@ using namespace std;
 /*
     multidimentional array
     0 - arrival time
-    1 - burst time
-    2 - copy of burst time
-    3 - waiting time
+    1 - copy of arrival time
+    2 - burst time
+    3 - copy of burst time
+    4 - waiting time
 */
 
 class queue{
-    int **rr, num, tq, min, time, tbt, i, *cirq, front, rear;
+    int **rr, num, tq, min, time, tbt, i, *cirq, front, rear, process;
     public:
     queue(){
         front = rear = -1;
@@ -18,36 +19,53 @@ class queue{
         cin >> num;
         rr = new int*[num];
         tbt = 0;
+        time = 0;
         for(i = 0; i < num; i++){
-            rr[i] = new int[4];
-            cout << "Enter the arrival time for Process" << i+1 << ":";
+            rr[i] = new int[10];
+            cout << "Enter the arrival time for Process " << i+1 << ":";
             cin >> rr[i][0];
-            cout << "Enter the burst time for Process" << i+2 << ":";
-            cin >> rr[i][1];
-            rr[i][2] = rr[i][1];
-            tbt += rr[i][1];
-            rr[i][3] = {0};
+            rr[i][1] = rr[i][0];
+            cout << "Enter the burst time for Process " << i+1 << ":";
+            cin >> rr[i][2];
+            rr[i][3] = rr[i][2];
+            tbt += rr[i][2];
         }
         cout << "Enter time quantum: ";
         cin >> tq;
-        cout << min;
-        time = minimum(rr[0]);
         cirq = new int[num];
         while(time<tbt){
             for(i = 0; i < num; i++){
-                if(rr[0][i] <= time)
-                    enqueue(i);
+                if(rr[i][3]>0){
+                    if(rr[i][1] <= time)
+                        enqueue(i);
+                }
             }
-            for()
+            for(i=front;i!=rear;++i%=num){
+                process = dequeue();
+                if(rr[process][3]>=tq){
+                    rr[process][3] -= tq;
+                    cout << "P" << process+1 << "-->";
+                    time += tq;
+                    rr[process] += tq;
+                }
+                else{
+                    time += rr[process][3];
+                    rr[process][3] = 0;
+                    cout << "P" << process+1 << "-->";
+                }
+            }
         }
     }
+    /*
     int minimum(int *a){
         min = a[0];
         for(int j = 0; j < sizeof(a)/sizeof(int); j++){
+            cout << a[j] << endl;
             if (min > a[j])
                 min = a[j];
         }
     }
+    */
     void enqueue(int a){
         if(front == -1){
             front = rear = 0;
@@ -58,14 +76,13 @@ class queue{
             cirq[rear] = a;
         }
     }
-    void dequeue(int a){
-        if(rear==front){
+    int dequeue(){
+        int k = front;
+        if(rear==front)
             rear = front = -1;
-        }
-        else{
+        else
             ++front %= num;
-        }
-        
+        return k;
     }
 
 };
