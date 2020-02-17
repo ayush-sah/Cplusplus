86ff13b872730d61a7cc282786bf1b89e21b4dcd
ayush-sah <ayush.sah@spit.ac.in>
2020-02-18 01:08:34 +0530

    Update johnson.cpp

--- Queue/johnson.cpp
+++ Queue/johnson.cpp
@@ -20,11 +20,42 @@ class queue{
             cout << "Enter execution time for Process " << table[i][0] << " on Machine 2 : ";
             cin >> table[i][2];
         }
+
+        cout << "\nThe values you entered are: " << endl;
+        cout << "\t|  PROCESS |\t| MACHINE 1 |\t| MACHINE 2 |" << endl;
+        cout << "     ___________________________________________________" << endl << endl;
+        for(int i = 0; i < num; i++)
+            cout << "\t|    P" << table[i][0] << "    |\t|     "<<    table[i][1] << "     |\t|     " << table[i][2] << "     |" << endl;
+        cout << endl;
+        /*
+        int count = 0;
         copy = table;
-        min = 0;
-        for(int i = 0; i < num; i++){
-            cout << copy[i][i];
+        while(count!=num){
+            min = 2147483647;
+            cout << "m " << min;
+            for(int i = 0; i < num; i++){
+                if(copy[i][1]<min && copy[i][1]!=0)
+                    min = copy[i][1];
+                if(copy[i][2]<min && copy[i][2]!=0)
+                    min = copy[i][2];
+            }
+            cout << "min " << min << endl;
+            for(int i = 0; i < num; i++){
+                if(min==copy[i][1]){
+                    front_enqueue(copy[i][0]);
+                    count++;
+                    delete[] copy[i];
+                }
+                else if(min==copy[i][2]){
+                    rear_enqueue(copy[i][0]);
+                    count++;
+                    delete[] copy[i];
+                }
+            }
+            cout << "count " << count << endl;
         }
+        display();
+        */
 
     }
     int minimum(){
@@ -35,6 +66,8 @@ class queue{
     void rear_enqueue(int process){q[--rev_rear] = process;}
 
     void display(){
+        for(int i = 0; i < num; i++)
+            cout << q[i] << " ";
     }
 };
 
