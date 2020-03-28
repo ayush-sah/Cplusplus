c55af2dedc78f4ca9e4a1aaa0bcfbbded6933f1e
ayush-sah <ayush.sah@spit.ac.in>
2020-03-28 14:02:50 +0530

    Update bst.cpp

--- Tree/bst.cpp
+++ Tree/bst.cpp
@@ -82,10 +82,95 @@ class bst{
         }
     }
     void traversal(){
+        int action, subaction;
+        cout << "\n1. Inorder\n2. Preorder\n3. Postorder\nEnter the type of traversal you want to perform: ";
+        cin >> action;
+        cout << "\n1. with Recursion\n2. without Recursion\nEnter which type of traversal you want to perform: ";
+        cin >> subaction;
+        switch(action){
+            case 1:
+                switch(subaction){
+                    case 1:
+                        cout << "Inorder traversal: ";
+                        inorder_rec(list);
+                        break;
+                    case 2:
+                        inorder_nonrec(list);
+                        break;
+                    default:
+                        cout << "Invalid option. Start Again.";
+                }
+                break;
+            
+            case 2:
+                switch(subaction){
+                    case 1:
+                        cout << "Preorder traversal: ";
+                        preorder_rec(list);
+                        break;
+                    case 2:
+                        preorder_nonrec(list);
+                        break;
+                    default:
+                        cout << "Invalid option. Start Again.";
+                }
+                break;
+            case 3:
+                switch(subaction){
+                    case 1:
+                        cout << "Postorder traversal: ";
+                        postorder_rec(list);
+                        break;
+                    case 2:
+                        postorder_nonrec(list);
+                        break;
+                    default:
+                        cout << "Invalid option. Start Again.";
+                }
+                break;
+            default:
+                cout << "Invalid option. Start Again.";
+        }
+    }
+
+    void inorder_rec(struct node *l){
+        if(l){
+            inorder_rec(l->lptr);
+            cout << l->data << " ";
+            inorder_rec(l->rptr);
+        }
+    }
+
+    void inorder_nonrec(struct node *l){
 
     }
-    void delete_e(){
 
+    void preorder_rec(struct node *l){
+        if(l){
+            cout << l->data << " ";
+            preorder_rec(l->lptr);
+            preorder_rec(l->rptr);
+        }
+    }
+
+    void preorder_nonrec(struct node *l){
+
+    }
+
+    void postorder_rec(struct node *l){
+        if(l){
+            postorder_rec(l->lptr);
+            postorder_rec(l->rptr);
+            cout << l->data << " ";
+        }
+    }
+
+    void postorder_nonrec(struct node *l){
+
+    }
+
+    void delete_e(){
+        
     }
     void search(){
 
